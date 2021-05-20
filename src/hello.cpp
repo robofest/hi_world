c62c0cd7af8f61ba2d9caad1aa90c0003ab192d3
CJ Chung <cchung@ltu.edu>
2021-05-20 11:51:35 -0400

    files for cpp added

---
+++ src/hello.cpp
@@ -0,0 +1,10 @@
+#include <ros/ros.h>
+int main (int argc, char **argv)
+{
+	ros::init(argc, argv, "hello_ros_node");
+	ros::NodeHandle nh;
+	ROS_INFO("Hello_ros_node started");
+	ros::Duration(5.0).sleep(); //seconds
+	ROS_INFO("Bye");
+}
+
