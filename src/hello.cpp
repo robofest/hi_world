0f90f9ef9cefb68d73e82373c31c6482488f602a
CJ Chung <cchung@ltu.edu>
2021-05-20 12:14:37 -0400

    hello.cpp updated

--- src/hello.cpp
+++ src/hello.cpp
@@ -4,7 +4,7 @@ int main (int argc, char **argv)
 	ros::init(argc, argv, "hello_ros_node");
 	ros::NodeHandle nh;
 	ROS_INFO("Hello_ros_node started");
-	ros::Duration(5.0).sleep(); //seconds
+	ros::Duration(3.0).sleep(); //seconds
 	ROS_INFO("Bye");
 }
 
