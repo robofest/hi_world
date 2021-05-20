f53d538d22f4f4c31e7f68f0b580100a0fe8a784
CJ Chung <cchung@ltu.edu>
2021-05-19 22:40:58 -0400

    1st change

--- scripts/hello.py
+++ scripts/hello.py
@@ -6,5 +6,5 @@ if __name__ == '__main__':
     
     rospy.init_node('hello_py_node')
     rospy.loginfo('hello_py_node started')
-    rospy.sleep(5)
+    rospy.sleep(3) # 5 to 3
     rospy.loginfo('hello_py_node ended')
